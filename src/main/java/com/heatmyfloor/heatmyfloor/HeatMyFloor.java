9f630f290b45f35227d28fee317877605ad8a52d
Wily Tatow <wrtad@ulaval.ca>
2025-10-10 18:38:44 +0100

    ajout de la vue le model et du controller

--- src/main/java/com/heatmyfloor/heatmyfloor/HeatMyFloor.java
+++ src/main/java/com/heatmyfloor/heatmyfloor/HeatMyFloor.java
@@ -4,6 +4,10 @@
 
 package com.heatmyfloor.heatmyfloor;
 
+import com.heatmyfloor.view.MainView;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 /**
  *
  * @author tatow
@@ -11,6 +15,18 @@ package com.heatmyfloor.heatmyfloor;
 public class HeatMyFloor {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+        
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("HeatMyfloor");
+
+            // Ajout de ton JPanel (MainView) dans la fenêtre
+            frame.setContentPane(new MainView());
+
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack(); // ajuste la taille automatiquement
+            frame.setLocationRelativeTo(null); // centre la fenêtre
+            frame.setVisible(true);
+        });
     }
 }
