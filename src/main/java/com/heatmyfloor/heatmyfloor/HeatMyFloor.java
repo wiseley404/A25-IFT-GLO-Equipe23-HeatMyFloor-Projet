faa82be06f4573461a173346689dcd6b03a25b6b
Wily Tatow <wrtad@ulaval.ca>
2025-10-10 18:02:29 +0100

    Ajout du fichier pom.xml et de la classe HeatMyFloor

---
+++ src/main/java/com/heatmyfloor/heatmyfloor/HeatMyFloor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.heatmyfloor.heatmyfloor;
+
+/**
+ *
+ * @author tatow
+ */
+public class HeatMyFloor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
