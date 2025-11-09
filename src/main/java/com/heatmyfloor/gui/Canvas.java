9a5ac50a4ba1ef5965c8f13010de07e313071b33
OUEDRAOGO Aliya Imann <aioue8@ulaval.ca>
2025-11-09 03:09:04 -0500

    modifications

--- src/main/java/com/heatmyfloor/gui/Canvas.java
+++ src/main/java/com/heatmyfloor/gui/Canvas.java
@@ -3,14 +3,23 @@ package com.heatmyfloor.gui;
 import javax.swing.*;
 import java.awt.*;
 import javax.swing.JLabel;
+<<<<<<< HEAD
 import java.io.Serializable;
 import com.heatmyfloor.domain.piece.PieceItemReadOnly;
 import com.heatmyfloor.gui.drawer.PieceDrawer;
 
 public class Canvas extends JPanel implements Serializable{
 
+=======
+//import java.awt.event.KeyAdapter;
+//import java.awt.event.KeyEvent;
+//import java.awt.event.MouseAdapter;
+//import java.awt.event.MouseEvent;
+//import com.heatmyfloor.domain.piece.Controller;
+>>>>>>> 6b83ba3 (modifications)
 
     private Rectangle rectangle;
+<<<<<<< HEAD
     private MainWindow mainWindow;
     private FormeIrregulierPanel dessinPanel;
     private PieceItemReadOnly itemSurvole;
@@ -20,7 +29,12 @@ public class Canvas extends JPanel implements Serializable{
     public Canvas(MainWindow mainWindow) {
         this.mainWindow = mainWindow;
     }
+=======
+    //private final Controller controller;
+
+>>>>>>> 6b83ba3 (modifications)
     public Canvas() {
+        //this.controller = controller;
         setBackground(Color.white);
         setLayout(null); // absolute positioning for placeholder icons
         setBorder(BorderFactory.createLineBorder(new Color(140, 140, 140), 2));
@@ -32,6 +46,7 @@ public class Canvas extends JPanel implements Serializable{
 //        add(toilet);
 //        JLabel cabinet = createIconLabel("/icons/cabinet.svg", 1020, 120, 60, 220);
 //        add(cabinet);
+<<<<<<< HEAD
               
     }
     
@@ -68,6 +83,33 @@ public class Canvas extends JPanel implements Serializable{
         int y = (getHeight() - largeur) / 2;
         rectangle = new Rectangle(x, y, longueur, largeur);
         repaint();
+=======
+        //setFocusable(true);
+        //ajouterListener();
+        
+    }
+    
+
+    //private void ajouterListener() {
+
+    //addKeyListener(new KeyAdapter() {
+     //   @Override
+      //  public void keyPressed(KeyEvent e) {
+           // if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE || e.getKeyCode() == KeyEvent.VK_DELETE) {
+        //        controller.supprimerItemSelectionne();
+             //   repaint();
+         //   }
+       // }
+   // });
+//}
+    
+    public void dessinerRectangle(int longueur, int largeur){
+      
+      int x = (getWidth() - longueur) / 2;
+      int y = (getHeight() - largeur) / 2;
+      rectangle = new Rectangle(x,y,longueur,largeur); 
+      repaint();
+>>>>>>> 6b83ba3 (modifications)
     }
     
    /* public Dimension getTailleRectangle() {
